5d472a31ebc32dfec8e8bf5f8ccc572c7bff6f11
Elmer Duron <elmerhduron@gmail.com>
2025-12-30 22:59:02 +0800

    add dml result

---
+++ src/main/java/com/junk/application/discordfeedhub/utils/DmlResult.java
@@ -0,0 +1,27 @@
+package com.junk.application.discordfeedhub.utils;
+
+/**
+ *
+ * @author elmerhd
+ */
+public record DmlResult (
+        DmlStatus status,
+        String message,
+        Throwable throwable){
+    
+    public boolean isSuccess() {
+        return status == DmlStatus.SUCCESS;
+    }
+
+    public static DmlResult success(int rowsAffected) {
+        return new DmlResult(
+                DmlStatus.SUCCESS,
+                rowsAffected + " row(s) affected",
+                null
+        );
+    }
+
+    public static DmlResult failure(DmlStatus status, String message, Throwable t) {
+        return new DmlResult(status, message, t);
+    }
+}
